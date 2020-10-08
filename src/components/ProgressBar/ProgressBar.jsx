dcfe5f82ce55ec5905815552340f26204312d947
Bachemedei <bronwyn.1992@gmail.com>
2020-10-08 15:11:51 +0800

    Fixed error in progress bar so it doesn't exceed 100%

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,35 +1,42 @@
-import React from "react";
-import "./ProgressBar.css";
+import React from "react"
+import "./ProgressBar.css"
 
 function ProgressBar({ data }) {
-    // Variables & State
-    const pledges = data.pledges;
+  // Variables & State
+  const pledges = data.pledges
 
-    // Helpers
-    const amountFunded = (pledges) => {
-      // Iterate through pledges array, add the amount of each pledge to total
-      return pledges.reduce((total, current) => {
-        total = total + current.amount;
-        return total;
-      }, 0);
-    };
-  
-    const percentComplete = (data, pledges) => {
-      let pledgeTotal = amountFunded(pledges)
-      return Math.floor(pledgeTotal/data.goal * 100)
-    };
-    const progressStyle = {
-      width: `${percentComplete(data, pledges)}%`,
-    };
+  // Helpers
+  const amountFunded = (pledges) => {
+    // Iterate through pledges array, add the amount of each pledge to total
+    return pledges.reduce((total, current) => {
+      total = total + current.amount
+      return total
+    }, 0)
+  }
+
+  const percentComplete = (data, pledges) => {
+    const pledgeTotal = amountFunded(pledges)
+    let percent = Math.floor((pledgeTotal / data.goal) * 100)
+    if (percent > 100) {
+      percent = 100
+    }
+    return percent
+  }
+
+  const progressStyle = {
+    width: `${percentComplete(data, pledges)}%`,
+  }
 
   return (
     <div className="progress-ctn">
       <div className="progress-bar">
         <div className="progress-complete" style={progressStyle}></div>
       </div>
-      <span className="progress-label">{percentComplete(data, pledges)}% of ${data.goal} goal reached!</span>
+      <span className="progress-label">
+        {percentComplete(data, pledges)}% of ${data.goal} goal reached!
+      </span>
     </div>
-  );
+  )
 }
 
-export default ProgressBar;
+export default ProgressBar
